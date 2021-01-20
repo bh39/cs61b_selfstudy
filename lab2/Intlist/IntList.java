f3e60411013abf9e89e57d10fd70f67765517b26
magicCC02 <hanbinghbhb@gmail.com>
2021-01-20 18:56:35 +0800

    finished lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList p = A;
+        while (p.rest != null) {
+            p = p.rest;
+        }
+        p.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,15 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList Q = new IntList(A.first, null);
+        IntList p = Q;
+        while (A.rest != null) {
+            A = A.rest;
+            p.rest = new IntList(A.first, null);
+            p = p.rest;
+        }
+        p.rest = B;
+        return Q;
     }
 
 
